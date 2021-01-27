9ba7426e1c8d313d439284aac7f30268203a8e1a
Andrew Downs <andrewdownsau@gmail.com>
2021-01-28 03:30:50 +1100

    Feature (Landmarks List): Card components, api calls (1. with just the location then 2. with specific details of landmarks)

--- src/components/page/Landmarks.jsx
+++ src/components/page/Landmarks.jsx
@@ -1,11 +1,38 @@
 import { useParams } from "react-router-dom";
-import {SubPageBody} from '../../styles/App'
+import { useEffect, useState} from 'react';
+import { SubPageBody } from '../../styles/App';
+import { LandmarkCard } from '../body/LandmarkCard/LandmarkCard'
+import {apiGet} from '../../api/openTripMap/apiGet'
 
 export function Landmarks() {
   let { location } = useParams();
+  const [landmarks, setLandmarks] = useState();
+
+  // Runs on ComponentDidMount once and will set the landmarks
+  useEffect(() => {
+    apiGet("geoname", "name=" + location).then(function(data) {
+      if (data.status === "OK") {
+        apiGet(
+          "radius",
+          `radius=1000&limit=5&offset=0&lon=${data.lon}&lat=${data.lat}&rate=2&format=json`
+        ).then(function(data) {
+          setLandmarks(data.map(item => item))
+        });
+      }
+    });
+  }, [location]);
+
   return (
     <SubPageBody>
       <h2>Landmarks for {location}</h2>
+      <div id="landmarks_list">
+        {landmarks && landmarks.map((landmark) =>
+          <LandmarkCard 
+            key={landmark.name} 
+            name={landmark.name}
+            id={landmark.xid}/>
+        )}
+      </div>
     </SubPageBody>
   );
 }
