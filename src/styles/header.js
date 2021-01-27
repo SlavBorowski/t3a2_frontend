1f7fd783ba88ff327cadae4a8297d8bbb646948c
Andrew Downs <andrewdownsau@gmail.com>
2021-01-27 22:03:04 +1100

    Fixed issue: Resizing of screen to small now showed centered background image and text

--- src/styles/header.js
+++ src/styles/header.js
@@ -4,11 +4,12 @@ export const BannerWrapper = styled.div`
   border: solid 0.1px white;
   height: 200px;
   background-image: url("/banner.jpg");
-  background-position: top-center;
+  background-position: center center;
   background-repeat: no-repeat;
   background-size: cover;
   text-align: center;
   color: white;
+
 `;
 
 
@@ -16,6 +17,16 @@ export const BannerText = styled.h1`
   background: rgba(0, 0, 0, 0.4);
   border: solid 2px white;
   width: 400px;
-  margin: 80px auto;
+  margin: 70px auto;
   padding: 15px;
+
+  @media (max-width: 530px) {
+    margin: 45px auto;
+    width: 60%;
+  }
+
+  @media (max-width: 400px) {
+    margin: 45px auto;
+    width: 220px;
+  }
 `;
