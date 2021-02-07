b6033efc1a2726731efc092c94fd3137e5811713
SlavBorowski <slav.borowski@outlook.com>
2021-02-07 16:34:51 +1100

    Created skeleton for private landmakr page.

--- src/components/loggedInPage/LandmarkPrivate.jsx
+++ src/components/loggedInPage/LandmarkPrivate.jsx
@@ -1,7 +1,43 @@
+import { PrivateTitle, 
+  PrivateDate,
+  PrivateText,
+  PrivatePageWrapper,
+  PrivateImageWrapper,
+  PrivateTextWrapper,
+  PrivateImagePlaceholder,
+  PrivateInfoWrapper } from "../../styles/LandmarkPrivate";
+
+
+
 export function LandmarkPrivate() {
   return (
     <>
-      <h1>LandmarkPrivate</h1>
+    <PrivatePageWrapper>
+      <PrivateTextWrapper>
+        <PrivateInfoWrapper>
+          <PrivateTitle>Landmark Name</PrivateTitle>
+          <PrivateDate>Date I last visited: 20/02/2021</PrivateDate>
+        </PrivateInfoWrapper>  
+        
+        <PrivateText>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</PrivateText>
+      </PrivateTextWrapper>
+
+      <PrivateImageWrapper>
+        <PrivateImagePlaceholder></PrivateImagePlaceholder>
+        <PrivateImagePlaceholder></PrivateImagePlaceholder>
+        <PrivateImagePlaceholder></PrivateImagePlaceholder>
+        <PrivateImagePlaceholder></PrivateImagePlaceholder>
+        <PrivateImagePlaceholder></PrivateImagePlaceholder>
+        <PrivateImagePlaceholder></PrivateImagePlaceholder>
+        <PrivateImagePlaceholder></PrivateImagePlaceholder>
+        <PrivateImagePlaceholder></PrivateImagePlaceholder>
+      </PrivateImageWrapper>
+    </PrivatePageWrapper>
     </>
   );
 }
